e65e1d859f1c4222ac4e19405bb01e806a43138e
wissammm <wissamboussella@gmail.com>
2025-08-28 19:40:52 +0200

    new data organisation

--- src/data_mon_dump.c
+++ src/data_mon_dump.c
@@ -6,44 +6,36 @@
 void DumpPartyMonData(struct Pokemon *mon, u32 *dst) {
     int i;
     
-    dst[0] = FALSE;  // isActive
-    dst[1] = GetMonData(mon, MON_DATA_SPECIES, NULL);
+    dst[0] = GetMonData(mon, MON_DATA_SPECIES, NULL);
+
+    dst[1] = FALSE;  // isActive
     dst[2] = GetMonData(mon, MON_DATA_ATK, NULL);
     dst[3] = GetMonData(mon, MON_DATA_DEF, NULL);
     dst[4] = GetMonData(mon, MON_DATA_SPEED, NULL);
     dst[5] = GetMonData(mon, MON_DATA_SPATK, NULL);
     dst[6] = GetMonData(mon, MON_DATA_SPDEF, NULL);
 
+    dst[7] = GetMonData(mon, MON_DATA_ABILITY_NUM, NULL);
+
+    dst[8] = gSpeciesInfo[dst[1]].types[0];  // type1 from species
+    dst[9] = gSpeciesInfo[dst[1]].types[1];  // type2 from species
     // Moves (sequential indices)
-    for (i = 0; i < MAX_MON_MOVES; i++)
-        dst[7 + i] = GetMonData(mon, MON_DATA_MOVE1 + i, NULL);
 
-    // IVs
-    dst[11] = GetMonData(mon, MON_DATA_HP_IV, NULL);
-    dst[12] = GetMonData(mon, MON_DATA_ATK_IV, NULL);
-    dst[13] = GetMonData(mon, MON_DATA_DEF_IV, NULL);
-    dst[14] = GetMonData(mon, MON_DATA_SPEED_IV, NULL);
-    dst[15] = GetMonData(mon, MON_DATA_SPATK_IV, NULL);
-    dst[16] = GetMonData(mon, MON_DATA_SPDEF_IV, NULL);
+    dst[10] = GetMonData(mon, MON_DATA_HP, NULL);
+    dst[11] = GetMonData(mon, MON_DATA_LEVEL, NULL);
+    dst[12] = GetMonData(mon, MON_DATA_FRIENDSHIP, NULL);
+    dst[13] = GetMonData(mon, MON_DATA_MAX_HP, NULL);
+    dst[14] = GetMonData(mon, MON_DATA_HELD_ITEM, NULL);
+    dst[15] = GetMonData(mon, MON_DATA_PP_BONUSES, NULL);
+    dst[16] = GetMonData(mon, MON_DATA_PERSONALITY, NULL);
+    dst[17] = GetMonData(mon, MON_DATA_STATUS, NULL);
 
-    // abilityNum (plus de isEgg)
-    dst[17] = GetMonData(mon, MON_DATA_ABILITY_NUM, NULL);
+    dst[18] = 0;// Status2
+    dst[19] = 0;// Status3
 
-    // Stats and attributes
-    dst[18] = 0;  // ability (placeholder)
-    dst[19] = gSpeciesInfo[dst[1]].types[0];  // type1 from species
-    dst[20] = gSpeciesInfo[dst[1]].types[1];  // type2 from species
-    dst[21] = GetMonData(mon, MON_DATA_HP, NULL);
-    dst[22] = GetMonData(mon, MON_DATA_LEVEL, NULL);
-    dst[23] = GetMonData(mon, MON_DATA_FRIENDSHIP, NULL);
-    dst[24] = GetMonData(mon, MON_DATA_MAX_HP, NULL);
-    dst[25] = GetMonData(mon, MON_DATA_HELD_ITEM, NULL);
-    dst[26] = GetMonData(mon, MON_DATA_PP_BONUSES, NULL);
-    dst[27] = GetMonData(mon, MON_DATA_PERSONALITY, NULL);
-    dst[28] = GetMonData(mon, MON_DATA_STATUS, NULL);
-    dst[29] = 0;// Status2
-    dst[30] = 0;// Status3
-    // PP (if needed, though moves were already stored)
-    for (i = 0; i < MAX_MON_MOVES; i++)
-        dst[31 + i] = GetMonData(mon, MON_DATA_PP1 + i, NULL);
+
+    for (i = 0; i < MAX_MON_MOVES; i+=2)
+        dst[20 + i] = GetMonData(mon, MON_DATA_MOVE1 + i, NULL);
+        dst[20 + i + 1] = GetMonData(mon, MON_DATA_PP1 + i, NULL);
+
 }
